4f682e99634143bc4556010411e8e882ef6b46db
Sergio Prada <chechopradaster@gmail.com>
2016-11-20 02:32:48 -0500

    Adds all models

---
+++ models/artist.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Artist = new Schema({
+  name: { type: 'String', required: true },
+  dateAdded: { type: 'Date', default: Date.now, required: true },
+});
+
+export default mongoose.model('Artist', Artist);
