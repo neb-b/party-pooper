e4e0371bb881cabf2d19d9eff3490bea54a37dba
Sergio Prada <chechopradaster@gmail.com>
2016-11-21 15:00:54 -0500

    Adds badpath tests for events

--- test/controllers/events.controller.spec.js
+++ test/controllers/events.controller.spec.js
@@ -1,14 +1,19 @@
 import td from 'testdouble';
 import _ from 'lodash';
+import Boom from 'boom';
 
 import EventController from '../../controllers/events.controller';
 
 describe('EventController', () => {
+  let controller;
+
   afterEach(() => td.reset());
 
   context('getEvents', () => {
+    beforeEach(() => controller = new EventController());
+    afterEach(() => td.reset());
+
     it('returns a list of events', (done) => {
-      let controller = new EventController();
       let events = [ 1, 2, 3, 4];
       let mockResponse = { send: td.function() };
       controller.Event = { find: td.function() };
@@ -18,11 +23,24 @@ describe('EventController', () => {
 
       controller.getEvents(_.noop, mockResponse, _.noop);
     });
+
+    it('returns a 404 if no events are found', (done) => {
+      let mockNext = td.function();
+      let mockError = Boom.notFound('No events found');
+      controller.Event = { find: td.function() };
+
+      td.when(controller.Event.find()).thenReject(mockError);
+      td.when(mockNext(mockError)).thenDo(() => done());
+
+      controller.getEvents(_.noop, _.noop, mockNext);
+    });
   });
 
   context('getEvent', () => {
+    beforeEach(() => controller = new EventController());
+    afterEach(() => td.reset());
+
     it('returns an event', (done) => {
-      let controller = new EventController();
       let event = { name: 'test-event' };
       let mockResponse = { send: td.function() };
       let mockRequest = { params: { id: 'test-id'} };
@@ -32,6 +50,18 @@ describe('EventController', () => {
       td.when(mockResponse.send(event)).thenDo(() => done());
 
       controller.getEvent(mockRequest, mockResponse, _.noop);
+    });
+
+    it('returns a 404 if no event is found', (done) => {
+      let mockNext = td.function();
+      let mockError = Boom.notFound('Event not found');
+      let mockRequest = { params: { id: 'test-id'} };
+      controller.Event = { findById: td.function() };
+
+      td.when(controller.Event.findById(mockRequest.params.id)).thenReject(mockError);
+      td.when(mockNext(mockError)).thenDo(() => done());
+
+      controller.getEvent(mockRequest, _.noop, mockNext);
     });
   });
 
