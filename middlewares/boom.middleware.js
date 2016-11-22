c649259832594964c5023cbade5e2f1895332476
Sergio Prada <chechopradaster@gmail.com>
2016-11-22 13:57:26 -0500

    Removes comments from boom middleware

--- middlewares/boom.middleware.js
+++ middlewares/boom.middleware.js
@@ -1,5 +1,3 @@
-// TODO sprada: A proper way to define different types
-// of status codes that come through Boom
 export default function (err, req, res, next) {
   if(err && err.output){
     const { payload, statusCode } = err.output;
