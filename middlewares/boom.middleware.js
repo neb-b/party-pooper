4801e443bd818d077e2bb09d5dd7a4458272b200
Sergio Prada <chechopradaster@gmail.com>
2016-11-22 13:20:15 -0500

    Adds error handleling first steps

--- middlewares/boom.middleware.js
+++ middlewares/boom.middleware.js
@@ -1,5 +1,7 @@
 // TODO sprada: A proper way to define different types
 // of status codes that come through Boom
 export default function (err, req, res, next) {
-  return res.status(500).send();
+  console.log(err); //Development help, remove for production
+  
+  return res.status(500).send(err);
 }
